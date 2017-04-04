8fb90081e9e4f78a13280e259e5c178690e22c05
Edgar Dewsbery <edewsbery@gmail.com>
2017-04-04 16:04:07 +0300

    db_connection initial commit

---
+++ database/db_connection.js
@@ -0,0 +1,22 @@
+const url = require('url');
+const Pool = require('pg').Pool;
+const env = require('env2')(./config.env);
+
+if(!process.env.DB_URL) {
+  throw new Error('Environment variable DB_URL must be set');
+}
+
+const params = url.parse(process.env.DB_URL);
+const [username, password] = params.auth.split(":");
+
+const config = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.MAX_CONNECTIONS || 2,
+  user: username,
+  password: password,
+  ssl: params.hostname !== 'localhost'
+};
+
+module.exports = new Pool(config);
