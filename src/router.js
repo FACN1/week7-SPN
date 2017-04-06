4661f5b5c69e6dfaa804f60f1d047a04d82b58dc
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-04-06 12:49:35 +0300

    joi validate

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const handlers = require('./handlers.js');
 const dbQueries = require('./db_queries.js');
+const joi = require('joi');
 
 const staticFiles = {
   method: 'GET',
@@ -47,6 +48,13 @@ const createPost = {
       }
       reply('You added a new post');
     });
+  },
+  config: {
+    validate: {
+      params: {
+        data: joi.string().alphanum()  
+      }
+    }
   }
 }
 
